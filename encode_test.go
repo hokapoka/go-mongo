4df0b3bddab499e07c2d0b52f295f01d759f48da
Gary Burd <gary@hello9.local>
2010-11-02 22:58:35 -0700

    add license

--- encode_test.go
+++ encode_test.go
@@ -1,12 +1,26 @@
+// Copyright 2010 Gary Burd
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
 package mongo
 
 import (
 	"bytes"
-    "testing"
+	"testing"
 )
 
 type encodeTest struct {
-	value  interface{}
+	value    interface{}
 	expected []byte
 }
 
@@ -75,12 +89,11 @@ func TestEncode(t *testing.T) {
 	for _, et := range encodeTests {
 		var buf bytes.Buffer
 		err := Encode(&buf, et.value)
-        actual := buf.Bytes()
-        if err != nil {
-            t.Errorf("error parsing %s: %s", et.value, err)
-        } else if !bytes.Equal(et.expected, buf.Bytes()) {
+		actual := buf.Bytes()
+		if err != nil {
+			t.Errorf("error parsing %s: %s", et.value, err)
+		} else if !bytes.Equal(et.expected, buf.Bytes()) {
 			t.Errorf("input=%s,\nexpected %q\nactual   %q", et.value, et.expected, actual)
-        }
+		}
 	}
 }
-
