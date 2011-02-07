4976a6d33ed27750d5dbcaac7b41695d231a900b
Gary Burd <gary@beagledreams.com>
2011-02-06 19:49:26 -0800

    Remove common commands.
    Because we will not have a complete list of commands, it's better to not have any.

--- helpers.go
+++ helpers.go
@@ -34,21 +34,6 @@ func FindOne(conn Conn, namespace string, query interface{}, options *FindOption
 	return cursor.Next(result)
 }
 
-// Commonly used database commands.
-var (
-	// List databases.
-	ListDatabasesCmd = Doc{{"listDatabases", 1}}
-
-	// Return last error.
-	GetLastErrorCmd = Doc{{"getLastError", 1}}
-
-	// Wait for server to flush data to disk and return last error.
-	GetLastErrorWaitFsyncCmd = Doc{{"getLastError", 1}, {"fsync", 1}}
-
-	// Wait for server to replicate data and return last error.
-	GetLastErrorWaitReplicasCmd = Doc{{"getLastError", 1}, {"w", 2}}
-)
-
 // RunCommand executes the command cmd on the database specified by the
 // database component of namespace.
 func RunCommand(conn Conn, namespace string, cmd, result interface{}) os.Error {
@@ -62,17 +47,19 @@ func RunCommand(conn Conn, namespace string, cmd, result interface{}) os.Error {
 	return FindOne(conn, db+".$cmd", cmd, nil, result)
 }
 
+var	getLastErrorCmd = Doc{{"getLastError", 1}}
+
 // GetLastError returns the last error for a database. The database is
 // specified by the database component of namespace. The command cmd is used to
-// fetch the last error. If cmd is nil, then GetLastErrorCmd is used as the
-// command. If the err argument is not nil, then err is returned directly from
-// the function.
+// fetch the last error. If cmd is nil, then the command {"getLasetError": 1}
+// is used to get the error.  If the err argument is not nil, then err is
+// returned directly from the function.
 func GetLastError(conn Conn, namespace string, cmd interface{}, err os.Error) os.Error {
 	if err != nil {
 		return err
 	}
 	if cmd == nil {
-		cmd = GetLastErrorCmd
+		cmd = getLastErrorCmd
 	}
 	var r map[string]interface{}
 	if err := RunCommand(conn, namespace, cmd, &r); err != nil {
@@ -101,8 +88,8 @@ type SafeConn struct {
 	// The connecion to wrap.
 	Conn
 
-	// The command document used to fetch the last error. If cmd is nil, then
-	// GetLastErrorCmd is used as the command.
+    // The command document used to fetch the last error. If cmd is nil, then
+    // the command {"getLastError": 1} is used as the command.
 	Cmd interface{}
 }
 
